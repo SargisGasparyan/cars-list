cfbdc484c16dceb073cd0a6092fc33ad334aa9ee
gpetikyan <petikyan.gevorg01@gmail.com>
2025-06-14 14:48:07 +0400

    Fixed Suspense worning

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -1,8 +1,11 @@
+import { Suspense } from 'react';
 
 import CarsList from '@/components/CarsList';
 
 export default function Page() {
   return (
+    <Suspense fallback={<div></div>}>
       <CarsList />
+    </Suspense>
   );
 }
